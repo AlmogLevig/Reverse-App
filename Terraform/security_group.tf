d48ba97555a789f1518b47bc91534c81516b03ea
AlmogLevig <100873275+AlmogLevig@users.noreply.github.com>
2022-10-03 01:31:08 +0300

    Update security_group.tf

--- Terraform/security_group.tf
+++ Terraform/security_group.tf
@@ -35,6 +35,13 @@ resource "aws_security_group" "ins_sg" {
   }
   
   ingress {
+    from_port   = 30000
+    to_port     = 32767
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  ingress {
     description      = "ssh connection"
     from_port        = 22
     to_port          = 22
